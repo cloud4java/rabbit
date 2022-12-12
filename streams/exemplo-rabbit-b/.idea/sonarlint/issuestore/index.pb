5d5b13f438c9e2b2d37d4bac67d2077c54a2fde4
Dorival Querino da Silva <dorival.querino@gmail.com>
2022-12-11 23:42:24 -0300

    Example C: Ssaving, sending to dlq, then desapears

---
+++ streams/exemplo-rabbit-b/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+b
+2src/main/java/org/example/FilaExemploListener.java,2\4\241f3e1c28e00f5e55a62b3c1d1b1e6049387227
+9
+	README.MD,9\6\968e80af9562e09cebe594968b130076952eafe1
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+o
+?src/main/java/org/example/FilaExemploReceiveObjectListener.java,7\b\7b39ad9c796a441bb50accd7641d398840f045e7
---
+++ streams/exemplo-rabbit-c/src/main/java/org/example/FilaExemploReceiveObjectListener.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import lombok.Data;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.MessageChannel;
+
+@EnableBinding(Sink2.class)
+public class FilaExemploReceiveObjectListener {
+
+    @StreamListener(Sink2.INPUT)
+    public void listen(UsuarioInput object){
+        System.out.println("Escutando um object = " + object);
+        if (object.getName() == null) {
+            //TODO: Postar na fila intemediária, por 3 vezes
+            throw new IllegalArgumentException("Name é obrigatório");
+        }
+    }
+
+}
+
+@Data
+class UsuarioInput{
+    private Integer id;
+    private String name;
+    private Double salary;
+}
+
+interface Sink2{
+    String INPUT = "simple-queue-with-dlq";
+
+    @Input(INPUT)
+    MessageChannel input();
+}
